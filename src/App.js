1ae59afd6d02824e85b87a6f73a454c366b40ff2
Kinrezc <matt.czernik@gmail.com>
2019-09-27 20:04:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { makeStyles, ThemeProvider } from '@material-ui/styles';
+import { motion, useAnimation, AnimatePresence } from 'framer-motion';
+import UniswapLogo from './assets/uniswap.png';
+import { Select, Modal } from '@material-ui/core';
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundImage:
+      'linear-gradient(320deg, rgba(254,0,176,0.32816876750700286) 0%, rgba(12,168,227,0.33) 100%)',
+    margin: 0,
+    position: 'absolute',
+    padding: 0,
+    height: '100%',
+    minHeight: 980,
+    minWidth: '100%',
+    top: 0,
+    left: 0,
+  },
+  appContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalBackground: {
+    position: 'absolute',
+    width: 750,
+    background: 'white',
+    opacity: 0.7,
+    height: 650,
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+  const [token, setToken] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const setModalOpen = () => {
+    setModalIsOpen(true);
+  };
+
+  const setModalClose = () => {
+    setModalIsOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={classes.root}>
+      <div className={classes.appContainer}>
+        <Select open={false} onClick={setModalOpen} />
+        <Modal
+          open={modalIsOpen}
+          onClose={setModalClose}
+          className={classes.modal}
         >
-          Learn React
-        </a>
-      </header>
+          <div className={classes.modalBackground}>Hello</div>
+        </Modal>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
