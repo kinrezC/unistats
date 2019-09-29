1a74b44bec952a1728514a99da9959cc853e23fe
Kinrezc <matt.czernik@gmail.com>
2019-09-28 20:55:02 -0400

    almost done with animations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import { makeStyles, ThemeProvider } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/styles';
 import { motion, useAnimation, AnimatePresence } from 'framer-motion';
 import UniswapLogo from './assets/uniswap.png';
 import BG from './assets/uniswap2.png';
@@ -9,7 +9,8 @@ import Mkr from './assets/Mkr';
 import Dai from './assets/Dai';
 import Spank from './assets/Spank';
 import Zrx from './assets/Zrx';
-import { Select, Modal, Grid } from '@material-ui/core';
+import Box from './assets/Box';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -34,6 +35,7 @@ const useStyles = makeStyles(theme => ({
   },
   contentContainer: {
     width: '50%',
+    minWidth: 700,
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flex-start',
@@ -44,13 +46,6 @@ const useStyles = makeStyles(theme => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  modalBackground: {
-    position: 'absolute',
-    width: 750,
-    background: 'white',
-    opacity: 0.7,
-    height: 650,
-  },
   uniswapLogo: {
     height: 300,
     width: 300,
@@ -58,6 +53,9 @@ const useStyles = makeStyles(theme => ({
   gridContainer: {
     height: 500,
     width: '100%',
+    minWidth: 700,
+    background: 'rgba(255, 255, 255, 0.2)',
+    borderRadius: 50,
   },
   logoContainers: {
     display: 'flex',
@@ -65,24 +63,45 @@ const useStyles = makeStyles(theme => ({
     alignItems: 'center',
   },
   logos: {
-    height: 96,
-    width: 96,
+    height: 80,
+    width: 80,
     '& > g': {
-      transform: 'scale(3)',
+      transform: 'scale(2.5)',
     },
   },
-  placeholder: {
-    marginTop: 50,
-    marginBottom: '25%',
-    height: 200,
-    width: 200,
-    backgroundColor: 'blue',
+  boxContainer: {
+    width: '100%',
+    height: 150,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 50,
+  },
+  box: {
+    position: 'relative',
+    top: '5%',
+    transform: 'scale(0.33)',
   },
 }));
 
 const App = () => {
   const classes = useStyles();
-  const constraintsRef = useRef(null);
+  const r1 = useRef(null);
+  const r2 = useRef(null);
+  const r3 = useRef(null);
+  const r4 = useRef(null);
+  const r5 = useRef(null);
+  const tokens = useAnimation();
+
+  useEffect(() => {
+    tokens.start(i => ({
+      y: [20, 0],
+      opacity: [0, 1],
+      transition: {
+        delay: i * 0.15 + 0.3,
+      },
+    }));
+  }, [tokens]);
 
   return (
     <div className={classes.root}>
@@ -95,59 +114,100 @@ const App = () => {
               alt="uniLogo"
             />
           </motion.div>
-          <Grid
-            container
-            direction="column"
-            justify="flex-start"
-            alignContent="center"
-            alignItems="center"
+          <motion.div
+            animate={{ opacity: [0, 1], scale: [0, 1] }}
+            transition={{ duration: 0.2 }}
             className={classes.gridContainer}
           >
-            <motion.div>
-              <div className={classes.placeholder} />
-            </motion.div>
             <Grid
               container
-              direction="row"
-              justify="space-between"
-              alignItems="center"
-              alignConent="center"
-            >
-              <Grid item xs={4} className={classes.logoContainers}>
-                <motion.div>
-                  <Dai className={classes.logos} />
-                </motion.div>
-              </Grid>
-              <Grid item xs={4} className={classes.logoContainers}>
-                <motion.div>
-                  <Zrx className={classes.logos} />
-                </motion.div>
-              </Grid>
-              <Grid item xs={4} className={classes.logoContainers}>
-                <motion.div>
-                  <Btc className={classes.logos} />
-                </motion.div>
-              </Grid>
-            </Grid>
-            <Grid
-              container
-              direction="row"
-              justify="space-evenly"
-              alignItems="center"
+              direction="column"
+              justify="flex-start"
               alignContent="center"
+              alignItems="center"
+              className={classes.grid}
             >
-              <Grid item xs={2} className={classes.logoContainers}>
-                <motion.div>
-                  <Spank className={classes.logos} />
-                </motion.div>
+              <motion.div className={classes.boxContainer}>
+                <Box className={classes.box} />
+              </motion.div>
+              <Grid
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="center"
+                alignConent="center"
+              >
+                <Grid item xs={4} className={classes.logoContainers}>
+                  <motion.div ref={r1} animate={tokens} custom={1}>
+                    <motion.div
+                      drag
+                      dragConstraints={r1}
+                      dragElastic={1}
+                      dragMomentum={false}
+                    >
+                      <Dai className={classes.logos} />
+                    </motion.div>
+                  </motion.div>
+                </Grid>
+                <Grid item xs={4} className={classes.logoContainers}>
+                  <motion.div ref={r3} animate={tokens} custom={3}>
+                    <motion.div
+                      drag
+                      dragConstraints={r3}
+                      dragElastic={1}
+                      dragMomentum={false}
+                    >
+                      <Zrx className={classes.logos} />
+                    </motion.div>
+                  </motion.div>
+                </Grid>
+                <Grid item xs={4} className={classes.logoContainers}>
+                  <motion.div ref={r5} animate={tokens} custom={5}>
+                    <motion.div
+                      drag
+                      dragConstraints={r5}
+                      dragElastic={1}
+                      dragMomentum={false}
+                    >
+                      <Btc className={classes.logos} />
+                    </motion.div>
+                  </motion.div>
+                </Grid>
               </Grid>
-              <Grid item xs={2} className={classes.logoContainers}>
-                <motion.div>
-                  <Mkr className={classes.logos} />
-                </motion.div>
+              <Grid
+                container
+                direction="row"
+                justify="space-evenly"
+                alignItems="center"
+                alignContent="center"
+              >
+                <Grid item xs={2} className={classes.logoContainers}>
+                  <motion.div ref={r2} animate={tokens} custom={2}>
+                    <motion.div
+                      drag
+                      dragConstraints={r2}
+                      dragElastic={1}
+                      dragMomentum={false}
+                    >
+                      <Spank className={classes.logos} />
+                    </motion.div>
+                  </motion.div>
+                </Grid>
+                <Grid item xs={2} className={classes.logoContainers}>
+                  <motion.div ref={r4} animate={tokens} custom={4}>
+                    <motion.div
+                      drag
+                      dragConstraints={r4}
+                      dragElastic={1}
+                      dragMomentum={false}
+                    >
+                      <Mkr className={classes.logos} />
+                    </motion.div>
+                  </motion.div>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
+          </motion.div>
         </div>
       </div>
     </div>
